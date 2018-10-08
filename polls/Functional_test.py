8bda9884f244af47befd5796c7965efae0a30821
Leonardo Valbuena <leov.calderon@gmail.com>
2018-10-07 22:12:20 -0500

    test registro verde

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -2,22 +2,26 @@ __author__ = 'asistente'
 from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common import action_chains, keys
+import time
 
 class FunctionalTest(TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome("/Users/leonardovalbuena/Downloads/chromedriver")
+        self.browser.set_window_size(1024, 768)
+        self.browser.implicitly_wait(5000)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_title(self):
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get('http://127.0.0.1:8000/#')
         self.assertIn('BuscoAyuda', self.browser.title)
 
-
     def test_registro(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://127.0.0.1:8000/#')
+
         link = self.browser.find_element_by_id('id_register')
         link.click()
 
@@ -31,7 +35,8 @@ class FunctionalTest(TestCase):
         experiencia.send_keys('5')
 
         self.browser.find_element_by_xpath(
-            "//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
+            "//select[@id='id_tiposDeServicio']/option[text()='TiposDeServicio object']").click()
+
         telefono = self.browser.find_element_by_id('id_telefono')
         telefono.send_keys('3173024578')
 
@@ -42,7 +47,7 @@ class FunctionalTest(TestCase):
         imagen.send_keys('/Users/leonardovalbuena/Downloads/developer.jpeg')
 
         nombreUsuario = self.browser.find_element_by_id('id_username')
-        nombreUsuario.send_keys('juan645')
+        nombreUsuario.send_keys('juan6454')
 
         clave = self.browser.find_element_by_id('id_password')
         clave.send_keys('clave123')
@@ -53,6 +58,9 @@ class FunctionalTest(TestCase):
         span = self.browser.find_element(By.XPATH, '//span[text()="Juan Daniel Arevalo"]')
 
         self.assertIn('Juan Daniel Arevalo', span.text)
+
+
+
 
 
 
